bc4c81fe972aeff28b910668d4a0b3c67b6a5395
Paul Kramme <pjkramme@gmail.com>
2016-05-26 20:07:45 +0200

    Better formating

--- threeorfive.java
+++ threeorfive.java
@@ -1,31 +1,23 @@
-
 public class threeorfive {
-
 	public static void main(String[] args) {
-		
-		
-		for(int b=0; b<100; b++){
-			
-			if((b%3==0) & (b%5==0)){System.out.println(b + " Fizz Buzz");			
+		for(int b = 0; b <= 100; b++)
+		{
+			if((b%3==0) & (b%5==0))
+			{
+				System.out.println(b + " Fizz Buzz");			
 			}
-			
-			else if(b%3==0){
+			else if(b%3==0)
+			{
 				System.out.println(b + " Fizz");
 			}
-		
-			else if(b%5==0){
+			else if(b%5==0)
+			{
 				System.out.println(b + " Buzz");
 			} 
-			else{ 
+			else
+			{ 
 				System.out.println(b);
 			}
 		}
-		
-			
 	}
-	
 }
-
-
-	
-
